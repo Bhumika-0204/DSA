72a302d0ac70e84be19b2c268e518bbc95784e84
Bhumika-0204 <bhumikakumari0802@gmail.com>
2025-11-15 09:12:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (84.95%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int  farthest = 0, curr = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+            farthest = max(farthest, i + nums[i]);
+
+            if (i == curr) {
+                jumps++;
+                curr = farthest;
+            }
+        }
+        return jumps;
+    }
+};
