e6c9c6df6161af18c48b154ad4e9e2867c805959
Bhumika-0204 <bhumikakumari0802@gmail.com>
2025-11-24 15:30:36 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (19.99%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,30 +1,42 @@
 class MinStack {
 public:
-    stack<int> st;
-    stack<int> helper;
+    stack<long long> st;
+    stack<long long> helper;
+    long long min;
     MinStack() {
-        
+         min=LLONG_MAX;
     }
     
     void push(int val) {
-        st.push(val);
-        if(helper.size()==0 || val<=helper.top()) helper.push(val);
-        else helper.push(helper.top());
+        long long x=(long long) val;
+        if(st.size()==0){
+            st.push(x);
+            min=x;
+        }
+        else if(x>=min) st.push(x);
+        else{
+            st.push(2*x-min);
+            min=x;
+        }
 
     }
     
     void pop() {
+        if(st.top()<min){
+            long long oldmin=2*min-st.top();
+            min=oldmin;
+        }
         st.pop();
-        helper.pop();
 
     }
     
     int top() {
-        return st.top();
+        if(st.top()<min) return (int)min;
+        else return (int)st.top();
     }
     
     int getMin() {
-        return helper.top();
+        return (int)min;
     }
 };
 
