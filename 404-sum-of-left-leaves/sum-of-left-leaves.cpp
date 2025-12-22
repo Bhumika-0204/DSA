2b22bfff385c2e6f7f9c66a62d86868d15a37298
Bhumika-0204 <bhumikakumari0802@gmail.com>
2025-12-22 13:29:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (23.91%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == NULL) return 0;
+
+        int sum = 0;
+
+        // Check if left child is a leaf
+        if (root->left &&
+            root->left->left == NULL &&
+            root->left->right == NULL) {
+            sum += root->left->val;
+        }
+
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
+
+        return sum;
+    }
+};
