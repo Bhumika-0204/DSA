0f7cb348b2931661d03e52c46e03bc935e4c8956
Bhumika-0204 <bhumikakumari0802@gmail.com>
2025-12-22 13:32:49 +0530

    Time: 8 ms (89.25%) | Memory: 15.8 MB (18.68%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> ans;
+
+    void dfs(Node* root) {
+        if (root == NULL) return;
+
+        ans.push_back(root->val);
+
+        for (Node* child : root->children) {
+            dfs(child);
+        }
+    }
+
+    vector<int> preorder(Node* root) {
+        dfs(root);
+        return ans;
+    }
+};
