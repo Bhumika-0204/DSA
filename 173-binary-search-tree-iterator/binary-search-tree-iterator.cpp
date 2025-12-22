0f9c675c7d7cdbd06030391efb0aba90983b5c10
Bhumika-0204 <bhumikakumari0802@gmail.com>
2025-12-22 13:16:37 +0530

    Time: 4 ms (52.25%) | Memory: 32 MB (34.37%) - LeetSync

---
+++ 173-binary-search-tree-iterator/binary-search-tree-iterator.cpp
@@ -0,0 +1,30 @@
+class BSTIterator {
+    stack<TreeNode*> st;
+
+    void pushLeft(TreeNode* node) {
+        while (node != NULL) {
+            st.push(node);
+            node = node->left;
+        }
+    }
+
+public:
+    BSTIterator(TreeNode* root) {
+        pushLeft(root);
+    }
+
+    int next() {
+        TreeNode* node = st.top();
+        st.pop();
+
+        if (node->right != NULL) {
+            pushLeft(node->right);
+        }
+
+        return node->val;
+    }
+
+    bool hasNext() {
+        return !st.empty();
+    }
+};
