f751a21e93c362c96370a00416eaf960561e0b8b
Bhumika-0204 <bhumikakumari0802@gmail.com>
2026-01-25 12:26:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (87.38%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    void getPerms(vector<int>&nums,int idx,vector<vector<int>>&ans){
-        if(idx==nums.size()){
-            ans.push_back({nums});
+    void recPermute(vector<int> &ds,vector<int> &nums,vector<vector<int>>&ans,vector<int> &freq){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
             return;
         }
-        for(int i=idx;i<nums.size();i++){
-            swap(nums[idx],nums[i]);
-            getPerms(nums,idx+1,ans);
-            swap(nums[idx],nums[i]);
+        for(int i=0;i<nums.size();i++){
+            if(freq[i]==0){
+                ds.push_back(nums[i]);
+                freq[i]=1;
+
+                recPermute(ds,nums,ans,freq);
+                freq[i]=0;
+                ds.pop_back();
+            }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        
         vector<vector<int>> ans;
-        getPerms(nums,0,ans);
+        vector<int> ds;
+        vector<int> freq(nums.size(),0);
+        
+        recPermute(ds,nums,ans,freq);
         return ans;
+        
     }
 };
