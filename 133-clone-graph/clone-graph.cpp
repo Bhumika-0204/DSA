efe24efff742bd65aa7f529e89ebb5efb2615474
Bhumika Kumari <bhumikakumari0802@gmail.com>
2026-02-03 13:55:20 +0530

    Time: 4 ms (34.23%) | Memory: 12.3 MB (28.35%) - LeetSync

---
+++ 133-clone-graph/clone-graph.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    Node* cloneGraph(Node* node) {
+        if (node == NULL) 
+            return NULL;
+        if (mp.find(node) != mp.end()) 
+            return mp[node];
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
+        for (Node* neigh : node->neighbors) {
+            clone->neighbors.push_back(cloneGraph(neigh));
+        }
+
+        return clone;
+    }
+};
