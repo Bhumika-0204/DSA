4ccc74e9cfbefcec264daabfc52488e9ec85e34a
Bhumika Kumari <bhumikakumari0802@gmail.com>
2026-02-17 02:35:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (21.58%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     vector<int> dp;
-    int ftd(vector<int> &arr,int i){
-        if(i==arr.size()-1) return arr[i];
-        if(i==arr.size()-2) return max(arr[i],arr[i+1]);
+    // int ftd(vector<int> &arr,int i){
+    //     if(i==arr.size()-1) return arr[i];
+    //     if(i==arr.size()-2) return max(arr[i],arr[i+1]);
 
-        if(dp[i]!=-1) return dp[i];
-        return dp[i]=max(arr[i]+ftd(arr,i+2),0+ftd(arr,i+1));
-    }
-    int rob(vector<int> &nums){
+    //     if(dp[i]!=-1) return dp[i];
+    //     return dp[i]=max(arr[i]+ftd(arr,i+2),0+ftd(arr,i+1));
+    // }
+    // int rob(vector<int> &nums){
+    //     dp.clear();
+    //     dp.resize(105,-1);
+    //     return ftd(nums,0);
+    // }
+    int fbu(vector<int>&arr){
         dp.clear();
-        dp.resize(105,-1);
-        return ftd(nums,0);
+        
+        int n=arr.size();
+        dp.resize(n);
+        if(n==1) return arr[0];
+        dp[n-1]=arr[n-1];
+        dp[n-2]=max(arr[n-1],arr[n-2]);
+        for(int i=n-3;i>=0;i--){
+            dp[i]=max(arr[i]+dp[i+2],0+dp[i+1]);
+        }
+        return dp[0];
+    }
+    int rob(vector<int>nums){
+        return fbu(nums);
     }
 };
