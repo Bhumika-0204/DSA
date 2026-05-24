104b32463ac8f7e2a3b88ab4fa641b2fe07bc01f
Bhumika Kumari <bhumikakumari0802@gmail.com>
2026-05-25 02:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (74.93%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        if(image[sr][sc]==color) return image;
+        if(image[sr][sc]==color) return image;//agr current pixel ka colour or final colour same h to infinite loop na fase isliye
         dfs(image,sr,sc,image[sr][sc],color);
         return image;
     }
     void dfs(vector<vector<int>>&image,int currRow,int currCol,int initialColour,int newColour){
         int n=image.size();
         int m=image[0].size();
-        if(currRow<0 || currCol<0 || currRow>=n || currCol>=m) return;
+        if(currRow<0 || currCol<0 || currRow>=n || currCol>=m) return;//boundary condi check 
 
-        if(image[currRow][currCol]!=initialColour) return;
+        if(image[currRow][currCol]!=initialColour) return; //colour match condi check
 
-        image[currRow][currCol]=newColour;
+        image[currRow][currCol]=newColour; //colouring
         dfs(image,currRow+1,currCol,initialColour,newColour);
         dfs(image,currRow,currCol+1,initialColour,newColour);
         dfs(image,currRow-1,currCol,initialColour,newColour);
