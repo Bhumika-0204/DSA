18bfa255fa30df82cccdf4d9c5a126d10683fd26
Bhumika Kumari <bhumikakumari0802@gmail.com>
2026-05-25 02:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (89.59%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int r, int c, int oldColor, int newColor) {
-        int n = image.size();
-        int m = image[0].size();
-
-        if (r < 0 || c < 0 || r >= n || c >= m) return;
-        if (image[r][c] != oldColor) return;
-
-        image[r][c] = newColor;
-
-        dfs(image, r + 1, c, oldColor, newColor);
-        dfs(image, r - 1, c, oldColor, newColor);
-        dfs(image, r, c + 1, oldColor, newColor);
-        dfs(image, r, c - 1, oldColor, newColor);
-    }
-
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int oldColor = image[sr][sc];
-        if (oldColor == color) return image;
-
-        dfs(image, sr, sc, oldColor, color);
+        if(image[sr][sc]==color) return image;
+        dfs(image,sr,sc,image[sr][sc],color);
         return image;
+    }
+    void dfs(vector<vector<int>>&image,int currRow,int currCol,int initialColour,int newColour){
+        int n=image.size();
+        int m=image[0].size();
+        if(currRow<0 || currCol<0 || currRow>=n || currCol>=m) return;
+
+        if(image[currRow][currCol]!=initialColour) return;
+
+        image[currRow][currCol]=newColour;
+        dfs(image,currRow+1,currCol,initialColour,newColour);
+        dfs(image,currRow,currCol+1,initialColour,newColour);
+        dfs(image,currRow-1,currCol,initialColour,newColour);
+        dfs(image,currRow,currCol-1,initialColour,newColour);
     }
 };
