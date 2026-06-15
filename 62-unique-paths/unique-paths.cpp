6cdfc44d1545bed3e0bfe865ae1755549a37b3d3
Bhumika Kumari <bhumikakumari0802@gmail.com>
2026-06-15 23:46:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.87%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    //using recursion
-    // int helper(int sr,int sc,int er,int ec){
-    //     if(sr==er&&sc==ec) return 1;
-    //     if(sr>er||sc>ec) return 0;
-    //     int downways=helper(sr+1,sc,er,ec);
-    //     int rightways=helper(sr,sc+1,er,ec);
-    //     return rightways+downways;
-    // }
-    // int uniquePaths(int m, int n) {
-    //     return helper(0,0,m-1,n-1);
-    // }
-    int helper(int sr,int sc,int er,int ec,vector<vector<int>>&dp){
-        if(sr==er&&sc==ec) return 1;
-        if(sr>er||sc>ec) return 0;
-        if(dp[sr][sc]!=-1) return dp[sr][sc];
-        return dp[sr][sc]=helper(sr+1,sc,er,ec,dp)+helper(sr,sc+1,er,ec,dp);
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return helper(0,0,m-1,n-1,dp);
+        vector<vector<int>>dp(m,vector<int>(n,1));
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+
+        }
+        return dp[m-1][n-1];
     }
 };
