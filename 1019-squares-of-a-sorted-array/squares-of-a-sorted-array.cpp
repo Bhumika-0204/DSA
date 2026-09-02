c9e621928a9ae53e0cc099e7f986d1cc2a0777e1
Bhumika Kumari <bhumikakumari0802@gmail.com>
2026-09-02 23:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 31.9 MB (9.20%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,51 +1,70 @@
 class Solution {
 public:
-    int getMax(vector<int>& arr) {
-        int max = arr[0];
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
-        }
-        return max;
-    }
-
-    void countSort(vector<int>& arr, int exp) {
-        vector<int> output(arr.size());
-        vector<int> count(10, 0);
-
-        for (int i = 0; i < arr.size(); i++) {
-            count[(arr[i] / exp) % 10]++;
-        }
-
-        for (int i = 1; i < 10; i++) {
-            count[i] += count[i - 1];
-        }
-
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
-            count[(arr[i] / exp) % 10]--;
-        }
-
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i] = output[i];
-        }
-    }
-
-    void radixSort(vector<int>& arr) {
-        int max = getMax(arr);
-
-        for (int exp = 1; max / exp > 0; exp *= 10) {
-            countSort(arr, exp);
-        }
-    }
-
     vector<int> sortedSquares(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] = nums[i] * nums[i];
+        vector<int>pos;
+        vector<int>neg;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=0) pos.push_back(nums[i]);
+            else neg.push_back(nums[i]);
+            
         }
-        radixSort(nums);
-        return nums;
+        int m=neg.size();
+        int n=pos.size();
+        vector<int>res(m+n);
+        if(m==0){
+            for(int i=0;i<nums.size();i++){
+                nums[i]*=nums[i];
+                
+            }
+            return nums;
+        }
+        else if(n==0){
+            for(int i=0;i<nums.size();i++){
+                nums[i]*=nums[i];
+
+            }
+            reverse(nums.begin(),nums.end());
+            return nums;
+        }
+        else{
+            for(int i=0;i<m;i++){
+                neg[i]*=neg[i];
+                
+                
+            }
+            reverse(neg.begin(),neg.end());
+            for(int i=0;i<n;i++){
+                pos[i]*=pos[i];
+
+            }
+            int id=0,i=0,j=0;
+            while(i<m && j<n){
+                if(pos[j]>neg[i]){
+                    res[id]=neg[i];
+                    i++;
+                    id++;
+                }
+                else{
+                    res[id]=pos[j];
+                    j++;
+                    id++;
+
+                }
+                
+            }
+            while(i<m){
+                    res[id]=neg[i];
+                    i++;
+                    id++;
+                }
+                while(j<n){
+                    res[id]=pos[j];
+                    j++;
+                    id++;
+                }
+                return res;
+        }
+        return res;
     }
 };
-
